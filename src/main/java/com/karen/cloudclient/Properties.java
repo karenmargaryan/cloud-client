7ddfea518e49f640771c3d30987bff29832bd7c7
Karen Margaryan <karenm@yahoo.com>
2020-03-24 15:34:48 +0400

    Fix the prefix

--- src/main/java/com/karen/cloudclient/Properties.java
+++ src/main/java/com/karen/cloudclient/Properties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "config")
+@ConfigurationProperties(prefix = "cloud-configuration")
 public class Properties {
 
     private String type;
